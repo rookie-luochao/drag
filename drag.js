b645bd77100cb8b3284bc035db91f0d065bda073
rookie-luochao <1055120207@qq.com>
2017-02-10 14:25:11 +0800

    commit first versions

--- drag.js
+++ drag.js
@@ -1,6 +1,16 @@
 function drag(id){
 	var obj=document.getElementById(id);
-	obj.onmousedown=function(){
-		
+	obj.onmousedown=function(ev){
+		var ev=ev||window.event;
+		var disX=ev.pageX-obj.offsetLeft;
+		var disY=ev.pageY-obj.offsetTop;
+		document.onmousemove=function(ev){
+			var ev=ev||window.event;
+			obj.style.left=ev.pageX-disX+'px';
+			obj.style.top=ev.pageY-disY+'px';
+		}
+		document.onmouseup=function(){
+			document.onmousemove=document.onmousedown=null;
+		}
 	}
 }
